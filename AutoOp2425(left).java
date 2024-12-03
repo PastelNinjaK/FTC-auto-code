641829ff70ab03d4d0e4cd3e38b7450532c2bb75
PastelNinjaK <bernalraven11@gmail.com>
2024-12-02 21:31:28 -0700

    Update AutoOp2425(left).java

--- AutoOp2425(left).java
+++ AutoOp2425(left).java
@@ -1,3 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
 @Autonomous(name = "Ultrasonic Sensor Auto Left", group = "Robot")
 public class AutoOp2425(left) extends LinearOpMode {
     private DcMotor leftDrive;
